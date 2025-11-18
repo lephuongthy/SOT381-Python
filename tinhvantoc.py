8eae8fbb858bd58914de266c667a60c36ff70842
lephuongthy <thy.lp.67khmt@ntu.edu.vn>
2025-11-18 11:36:28 +0700

    tính vận tốc của vật

---
+++ tinhvantoc.py
@@ -0,0 +1,5 @@
+# Để tính vận tốc của vật khi rơi tự do từ một độ cao, bạn có thể sử dụng công thức \(v=\sqrt{2gh}\)
+from math import sqrt
+h=float(input("nhap do cao h: "))
+g=9.8
+print(f"van toc cua vat tu do cao h la: {sqrt(2*g*h)}")
