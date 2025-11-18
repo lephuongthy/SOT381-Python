c638b1fd3b406b9a66fac666924cccaf6334ab12
lephuongthy <thy.lp.67khmt@ntu.edu.vn>
2025-11-18 11:40:33 +0700

    tính diện tích hình thang

---
+++ dientichhinhthang.py
@@ -0,0 +1,7 @@
+a=float(input("nhap do dai day lon: "))
+b=float(input("nhap do dai day be: "))
+c=float(input("nhap chieu cao: "))
+if a>0 and b>0 and c>0:
+    print(f"dien tich hinh thang la: {((a+b)*c)/2}")
+else:
+    print(f"khong hop le")
