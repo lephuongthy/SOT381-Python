ae7a876e6f518ec638e6cfd5ec2daeb164326dc5
lephuongthy <thy.lp.67khmt@ntu.edu.vn>
2025-12-17 20:04:11 +0700

    Update tong_chu_so.py

--- tong_chu_so.py
+++ tong_chu_so.py
@@ -1,6 +1,7 @@
 #tính tổng các chữ số của số nguyên dương n
 n=int(input("nhập một số bất kì: "))
-a=[]
+total=0
 for i in str(n):
-    a.append(i)
-print(f"tổng chữ số của số {n} là {len(a)}")
+    total+=int(i)
+print(f"tổng của các chữ số của {n} là {total}")
+
