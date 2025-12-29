8fa6923f9f5e81aedeefebd99039df76bc60bbdf
lephuongthy <thy.lp.67khmt@ntu.edu.vn>
2025-12-29 15:29:41 +0700

    Update cau3_A1.py

--- cau3_A1.py
+++ cau3_A1.py
@@ -1,4 +1,3 @@
-#nhập một danh sách N số nguyên và in ra số lượng phần tử có giá trị chẵn
 n=int(input("nhập giới hạn n của danh sách: "))
 a=[]
 b=[]
@@ -13,4 +12,3 @@ for u in a:
     if u%2==0:
         b.append(u)
 
-print(f"số lượng phần tử có giá trị chẵn là: {len(b)}")
